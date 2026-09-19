fc894c1e76af758b2384e17688f035ad332520c2
NahuBor <nahuelemilianoborja33@gmail.com>
2026-09-19 02:25:28 -0300

    Add data seeding before starting Uvicorn
    Added initial data seeding before starting the API.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
+# 1. Aplicar las migraciones de Alembic (crea las tablas)
 alembic upgrade head
+
+# 2. Poblado de datos iniciales
+python seed.py
+
+# 3. Iniciar la API con Uvicorn
 exec uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
